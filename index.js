64d4201a65d10c5d321738a8101e570533f2b1ad
KaylumCassidy <102676296+KaylumCassidy@users.noreply.github.com>
2023-07-21 10:21:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
+<script>
+
+  var firebaseConfig = {
+    apiKey: "Test",
+    authDomain: "Test.firebaseapp.com",
+    databaseURL: "https://Test.firebaseio.com",
+    projectId: "Test",
+    storageBucket: "Test.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID", //Dont know where
+    appId: "Test"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+</script>
